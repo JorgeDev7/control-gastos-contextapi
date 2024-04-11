524e9611e127fe27d0dbb8350d54f6a9f12c3738
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-04-11 13:25:07 -0600

    Adding budget with an action to global state

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,10 @@
-import { useState, ChangeEvent, useMemo } from "react";
+import { useState, ChangeEvent, useMemo, FormEvent } from "react";
+import { useBudget } from "../hooks/useBudget";
 
 export default function BudgetForm() {
 
     const [budget, setBudget] = useState(0);
+    const { dispatch } = useBudget();
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setBudget(+e.target.value);
@@ -12,8 +14,22 @@ export default function BudgetForm() {
         return isNaN(budget) || budget <= 0;
     }, [budget]);
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        dispatch({
+            type: 'add-budget',
+            payload: {
+                budget
+            }
+        });
+    };
+
     return (
-        <form className="space-y-5">
+        <form
+            className="space-y-5"
+            onSubmit={handleSubmit}
+        >
             <div className="flex flex-col space-y-5">
                 <label
                     htmlFor="budget"
